ef42c8f4daf97ac7cb97b968cb860abb92699f97
Jasmine <jasmineang@hotmail.com>
2017-03-31 21:36:03 +0800

    Add initial routing

--- public/app.js
+++ public/app.js
@@ -1,1 +1,13 @@
 var todoApp = angular.module('todoApp', ['ui.router']);
+
+todoApp.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: '/views/home.partial.html'
+        })
+        
+});
