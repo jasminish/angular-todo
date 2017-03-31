06725480f22db7cbf4a628e700658693e3ba4628
Jasmine <jasmineang@hotmail.com>
2017-04-01 01:14:53 +0800

    Implement saving of tasks to local storage

--- public/todo.controller.js
+++ public/todo.controller.js
@@ -1,26 +1,39 @@
 angular.module('todoApp')
     .controller('todoController', function($scope){
-        $scope.tasks = []; 
-        $scope.completeCount = 0; 
+        $scope.saved = localStorage.getItem('tasks');
+        $scope.tasks = $scope.saved !== null ? JSON.parse($scope.saved) : [];
+        localStorage.setItem('tasks', JSON.stringify($scope.tasks));
+        console.log(localStorage.getItem('tasks'));
+        
+        $scope.completeCount = function() { 
+            let count = 0; 
+            $scope.tasks.forEach(function(t) {
+                if (t.completed === true) count++;
+            });
+            return count; 
+        }();
         $scope.showCompleted = false; 
 
         $scope.add = function() {
-            var t = {
+            let t = {
                 name: $scope.taskName,
                 completed: false
             };
             $scope.tasks.push(t);
             $scope.taskName = ""; 
+            localStorage.setItem('tasks', JSON.stringify($scope.tasks));
         }
         $scope.toggleComplete = function(t) {
             if (t.completed) 
                 $scope.completeCount++;
             else 
                 $scope.completeCount--;
+            localStorage.setItem('tasks', JSON.stringify($scope.tasks));
         }
         $scope.destroy = function(t) {
             let index = $scope.tasks.indexOf(t); 
             $scope.tasks.splice(index, 1);
+            localStorage.setItem('tasks', JSON.stringify($scope.tasks));
         }
 
         $scope.toggleFilter = function() {
@@ -29,6 +42,7 @@ angular.module('todoApp')
     })
     .filter('completedFilter', function() {
         return function(tasks, show) {
+            console.log(tasks);
             if (!show) {
                 var result = [];
                 tasks.forEach(function(t) {
