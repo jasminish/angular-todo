af2cf2557c03359fa7f3df3907977de609dcdf61
Jasmine <jasmineang@hotmail.com>
2017-03-31 22:57:11 +0800

    Implement todolist

--- public/app.js
+++ public/app.js
@@ -9,5 +9,9 @@ todoApp.config(function($stateProvider, $urlRouterProvider) {
             url: '/home',
             templateUrl: '/views/home.partial.html'
         })
-        
+        .state('todo', {
+            url: '/todo',
+            templateUrl: '/views/todo.partial.html',
+            controller: 'todoController'
+        })
 });
